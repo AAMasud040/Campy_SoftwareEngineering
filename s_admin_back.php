7413077bc21e6488c0383450181ce2e360cfd1c5
Anamulfahad <ahasan192110@bscse.uiu.ac.bd>
2022-03-27 22:01:38 +0600

    code for university status input box and buttons

--- s_admin_back.php
+++ s_admin_back.php
@@ -17,5 +17,20 @@
                
          </form>";
     }
+
+    // university status
+    if(isset($_POST['Uni_status']))
+    {
+         echo "<form action='' method='POST'>
+         <input name='university_id' type='text' class='input-box' placeholder='university id' required name='' id=''>
+         <button name='deactive_s' type='submit' class='submit-btn'>Deactive</button><br>
+         </form>";
+         echo "<form action='' method='POST'>
+         <input name='university_id' type='text' class='input-box' placeholder='university id' required name='' id=''>
+         <button name='active_s' type='submit' class='submit-btn'>Active</button>     
+         </form>";
+
+     
+    }
    }
 ?>
