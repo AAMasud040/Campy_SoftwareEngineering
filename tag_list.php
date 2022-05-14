2f2c45bf2c95bdf87c85751f936ee62b4cba2943
Arindam Kundu Amit <aamit192107@bscse.uiu.ac.bd>
2022-05-14 12:48:40 +0600

    table created for showing tag list

--- tag_list.php
+++ tag_list.php
@@ -4,5 +4,24 @@
     $qry = "SELECT * FROM tag";
     $result = mysqli_query($conn, $qry);
     $count = mysqli_num_rows($result);
+    if($count==0){
+        echo "<h2>No Tag in Database<h2>";
+    }
+    else{
+        echo"<table>
+        <tr>
+          <th>Tag Name</th>
+        </tr>";
+        while($row = mysqli_fetch_assoc($result)){
+            $tag_name = $row['tag_name'];
+            echo" <div class='admin_form'>";
+            echo "<t
+                <td><input type='hidden' name='tag_name' value='$tag_name'/>$tag_name</input></td>
+                </tr>";
+           echo "</div>";
+        }
+    }
+
+   
 
 ?>
