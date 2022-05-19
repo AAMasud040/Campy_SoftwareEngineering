a2d39726d9e39326f104540c0ef43672f979991d
Anamulfahad <ahasan192110@bscse.uiu.ac.bd>
2022-05-19 09:45:00 +0600

    html code for profile view

--- Student/student_back.php
+++ Student/student_back.php
@@ -15,4 +15,13 @@
     $qry2 = "SELECT * FROM university WHERE university_id = $University_id";
     $result2 = mysqli_query($conn, $qry2);
     $row2 = mysqli_fetch_assoc($result2);
+
+    $university_name = $row2['university_name'];
+
+    $conntent = "<div id='profileView'>
+                 <h1>Name: $name</h1>
+                 <h3>Student Id: $id<h3>
+                 <h3>Your Email: $email<h3>
+                 <h3>Student of $university_name<h3>
+                 </div>";
 ?>
