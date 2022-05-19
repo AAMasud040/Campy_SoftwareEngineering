7a15693bc22cc491b1d4d339782974504185c811
Anamulfahad <ahasan192110@bscse.uiu.ac.bd>
2022-05-19 09:33:29 +0600

    fecth from student table and query to select all from university

--- Student/student_back.php
+++ Student/student_back.php
@@ -2,6 +2,15 @@
     include "db_connect.php";
 
     $id = 011183040;
-    $qry = "SELECT * FROM student WHERE user_id = $id"
-    
+    $qry = "SELECT * FROM student WHERE user_id = $id";
+    $result = mysqli_query($conn, $qry);
+    $row = mysqli_fetch_assoc($result);
+
+    $name = $row['user_name'];
+    $email = $row['email'];
+    $University_id = $row['Universityuniversity_id'];
+    $approval = $row['approval'];
+    $status = $row['status'];
+
+    $qry2 = "SELECT * FROM university WHERE university_id = $University_id"
 ?>
