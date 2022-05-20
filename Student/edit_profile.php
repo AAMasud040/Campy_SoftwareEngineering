807f16e2b094b1078dccd0dc7599b2619cad67c6
Anamulfahad <ahasan192110@bscse.uiu.ac.bd>
2022-05-21 00:38:15 +0600

    query for all work and personal information

--- Student/edit_profile.php
+++ Student/edit_profile.php
@@ -4,6 +4,33 @@
           include "db_connect.php";
 
           $id = '011183040';
+         $qry = "SELECT * FROM student WHERE user_id = $id";
+         $result = mysqli_query($conn, $qry);
+         $row = mysqli_fetch_assoc($result);
+
+         $name = $row['user_name'];
+         $email = $row['email'];
+         $University_id = $row['Universityuniversity_id'];
+         $approval = $row['approval'];
+         $status = $row['status'];
+
+         $qry2 = "SELECT * FROM university WHERE university_id = $University_id";
+         $result2 = mysqli_query($conn, $qry2);
+         $row2 = mysqli_fetch_assoc($result2);
+
+         $university_name = $row2['university_name'];
+
+         $qry3 = "SELECT * FROM student_work_profile WHERE student_id = $id";
+         $result3 = mysqli_query($conn, $qry3);
+         $count = mysqli_num_rows($result3);
+         $row3 = mysqli_fetch_assoc($result3);
+         if($count == 0){
+             $skills = '';
+             $github = '';
+             $linkedIn = '';
+             $facebook = '';
+
+         }
           
 
           if(isset($_POST['edit_profile'])){
