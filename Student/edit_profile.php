70d52b11c0d35f0d4e4839e0558f956afaec15d7
Anamulfahad <ahasan192110@bscse.uiu.ac.bd>
2022-05-21 02:48:00 +0600

    query to update student table

--- Student/edit_profile.php
+++ Student/edit_profile.php
@@ -11,8 +11,7 @@
          $name = $row['user_name'];
          $email = $row['email'];
          $University_id = $row['Universityuniversity_id'];
-         $approval = $row['approval'];
-         $status = $row['status'];
+         $password = $row['user_pass'];
 
          $qry2 = "SELECT * FROM university WHERE university_id = $University_id";
          $result2 = mysqli_query($conn, $qry2);
@@ -31,27 +30,61 @@
              $facebook = '';
 
          }
+
+          else{
+             $skills = $row3['skills'];
+             $github = $row3['github'];
+             $linkedIn = $row3['linkedIn'];
+             $facebook = $row3['facebook'];
+          }
           
 
           if(isset($_POST['edit_profile'])){
                echo "<form action='' method='POST'>
-                    <input name='user_name' type='text' class='input-box' value ='$name'>
+                    <input name='name' type='text' class='input-box' value ='$name'>
      
-                    <input name='gmail' type='text' class='input-box' value ='$email'>
-                    <input name='gmail' type='text' class='input-box' value ='$skills'>
-                    <input name='gmail' type='text' class='input-box' value ='$github'>
-                    <input name='gmail' type='text' class='input-box' value ='$linkedIn'>
-                    <input name='gmail' type='text' class='input-box' value ='$facebook'>
-
-                    <input name='old_pass' type='password' class='input-box' placeholder='Old password' required name=''>
-                    <input name='pass' type='password' class='input-box' placeholder='new password' required name=''>
-                    <button name='add_uni' type='submit' class='submit-btn'>Save</button>
+                    <input name='email' type='text' class='input-box' value ='$email'>
+                    <input name='skills' type='text' class='input-box' value ='$skills'>
+                    <input name='github' type='text' class='input-box' value ='$github'>
+                    <input name='linkedIn' type='text' class='input-box' value ='$linkedIn'>
+                    <input name='facebook' type='text' class='input-box' value ='$facebook'>
+                    <input name='new_pass' type='password' class='input-box' placeholder='new password' value =''>
+                    <input name='old_pass' type='password' class='input-box' placeholder='Old password' value =''>
+                    <button name='save' type='submit' class='submit-btn'>Save</button>
      
                     </form>";
           }
 
-          if(isset($_POST['edit_profile'])){
-               $new_name = $_POST['admin_u_name'];
+          if(isset($_POST['save'])){
+               $new_name = $_POST['name'];
+               $new_email = $_POST['email'];
+               $new_skills = $_POST['skills'];
+               $new_github = $_POST['github'];
+               $new_linkedIn = $_POST['linkedIn'];
+               $new_facebook = $_POST['facebook'];
+               $old_pass = $_POST['old_pass'];
+               $new_pass = $_POST['new_pass'];
+               
+               if($old_pass != $password){
+                    echo "<script>alert('you have to enter the correct password to save changes')</script>";
+               }
+
+               else{
+                    $insert = "UPDATE student
+                              SET user_name = '$new_name', email= '$new_email'
+                              WHERE user_id = $id";
+                    $result_insert = mysqli_query($conn, $insert);
+
+                    if($new_pass !=''){
+                         $insert = "UPDATE student
+                              SET user_pass = '$new_pass'
+                              WHERE user_id = $id";
+                    $result_insert = mysqli_query($conn, $insert);
+                    }
+
+                     echo "<script>alert('Profile successfully updated')</script>";
+               }
+               
           }
 
           
